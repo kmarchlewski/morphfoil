60e2261a41471459dbb63e2986958cbd4e0cac58
kmarchlewski <kmarchlewski@meil.pw.edu.pl>
2020-10-05 13:09:30 +0200

    morphfoil documentation updated

--- R/morphfoil_pkg.R
+++ R/morphfoil_pkg.R
@@ -1,10 +1,21 @@
 
-#' morphfoil: A package for morphing meshes in a .msh2 format
+#' morphfoil: Tools for morphing meshes in a .msh2 format
 #'
-#' @description The package contains a collection of functions for morphing
-#'    computational meshes in a .msh2 format for a flow over airfoils.
-#'    Meshes in the .msh2 format are used by xred solver developed at MEiL PW.
+#' @description The package contains a collection of functions which allow morphing
+#'    and plotting of computational meshes in a .msh2 format for a flow over airfoils.
+#'    Meshes in the .msh2 format are used by \code{xred} solver developed at MEiL PW.
 #'
+#' @section morphfoil functions:
+#'    \itemize{
+#'      \item{\code{\link[morphfoil]{select_constr}}}
+#'      \item{\code{\link[morphfoil]{select_morphp}}}
+#'      \item{\code{\link[morphfoil]{morph_mesh}}}
+#'      \item{\code{\link[morphfoil]{plot_mesh}}}
+#'      \item{\code{\link[morphfoil]{plot_airfoil}}}
+#'      \item{\code{\link[morphfoil]{read_mesh}}}
+#'      \item{\code{\link[morphfoil]{write_mesh}}}
+#'      \item{\code{\link[morphfoil]{read_airfoil}}}
+#'    }
 #' @docType package
 #' @name morphfoil
 
